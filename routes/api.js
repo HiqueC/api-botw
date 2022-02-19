2ae2bdb324e173e2cc9913ce6e6f6a025c69034b
HiqueC <55243695+HiqueC@users.noreply.github.com>
2022-02-19 15:21:02 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -158,7 +158,7 @@ loghandler = {
         mensagem: 'talvez esteja sendo consertado'
     }
 }
-const listkey = ["supraz", "yuna", "lhannaBot"];
+const listkey = ["supraz", "yuna", "lhannaBot", "hiquec"];
 var len = 15
         var arr = '123456789abcdefghijklmnopqrstuvwxyz'
         var random = '';
