06917024e11f1ae729ff22d56cf00d95c0fa6dd3
HiqueC <55243695+HiqueC@users.noreply.github.com>
2022-02-19 14:24:07 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -158,7 +158,7 @@ loghandler = {
         mensagem: 'talvez esteja sendo consertado'
     }
 }
-const listkey = ["hiquec", "cg5f@"];
+const listkey = ["hiquec", "cg5f@", "punheta"];
 var len = 15
         var arr = '123456789abcdefghijklmnopqrstuvwxyz'
         var random = '';
